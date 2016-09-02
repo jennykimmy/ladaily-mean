b420f6468f8b120c0e2c1b1ecb46a6e2c6b04985
jenny kim <ms.jennykimmy@gmail.com>
2016-09-02 14:59:59 -0700

    initial commit

---
+++ config/database.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+// Use different database URIs based on whether an env var exists.
+var dbUri = process.env.MONGODB_URI ||
+            'mongodb://localhost/apitodos';
+
+if (!process.env.MONGODB_URI) {
+  // check that MongoD is running...
+  require('net').connect(27017, 'localhost').on('error', function() {
+    console.log("Yo, Turn on Mongod!!");
+    process.exit(0);
+  });
+}
+
+mongoose.connect(dbUri);
+
+module.exports = mongoose;
